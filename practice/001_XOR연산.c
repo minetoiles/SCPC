5fc53aaf6245a4ab018519c791127f0f2e32e432
minetoiles <163957799+minetoiles@users.noreply.github.com>
2025-07-01 23:13:31 +0900

    Create 001_XOR연산.c

---
+++ practice/001_XOR연산.c
@@ -0,0 +1,70 @@
+초등학교교 학생인 정우와 석환이는 최근 학교에서 두 이진수의 XOR 연산에 대해 배웠다.
+둘은 매우 영특한 학생이라 새로 배운 연산을 갖고 이리저리 장난치기 시작했다.
+다만 석환이는 정우에게 일을 시키는 것을 좋아하는지라 다음과 같은 제안을 했다.
+“내가 N개의 10진수를 주면, 등장하는 숫자들 중 홀수번만 나타나는 숫자들을 모두 XOR한 결과를 구해줘.”
+
+예를 들어 '2, 5, 3, 3' 이 주어질 경우, '2'와'5'는 1번(홀수 번) 나타나고 '3' 은 2번 (짝수 번) 나타나므로
+홀수 번 나타난 '2' 와 '5'를 XOR한 결과를 구해야 하고,
+'2, 5, 3, 3, 2, 4, 5, 3' 이 주어질 경우 '2' 와 '5' 는 2번 나타나고, '3' 은 3번, '4' 는 1번 나타나므로
+홀수 번 나타난 '3' 과 '4'를 XOR한 결과를 구해야 한다.
+
+정우는 제안을 수락했지만, 가면 갈수록 매번 XOR 연산을 수행하는 일에 지치고 있다.
+정우를 도와서 주어 진 문제를 해결하는 프로그램을 작성하라.
+
+tip) XOR연산은 같은 숫자가 나오면 어차피 0으로 처리되기 때문에 홀짝 구분 없이 풀어도 된다!
+
+
+#include <stdio.h>
+
+int Answer;
+
+int main(void)
+{
+	int T, test_case;
+	/*
+	   The freopen function below opens input.txt file in read only mode, and afterward,
+	   the program will read from input.txt file instead of standard(keyboard) input.
+	   To test your program, you may save input data in input.txt file,
+	   and use freopen function to read from the file when using scanf function.
+	   You may remove the comment symbols(//) in the below statement and use it.
+	   But before submission, you must remove the freopen function or rewrite comment symbols(//).
+	 */
+	// freopen("input.txt", "r", stdin);
+
+	/*
+	   If you remove the statement below, your program's output may not be rocorded
+	   when your program is terminated after the time limit.
+	   For safety, please use setbuf(stdout, NULL); statement.
+	 */
+	setbuf(stdout, NULL);
+
+	scanf("%d", &T);
+	for(test_case = 0; test_case < T; test_case++)
+	{
+		
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		/*
+		   Implement your algorithm here.
+		   The answer to the case will be stored in variable Answer.
+		 */
+		 
+		 int num;
+		 scanf("%d", &num);
+		 Answer = 0;
+		 int i;
+		 for (i = 0; i < num; i++){
+		     int n;
+		     scanf("%d", &n);
+		     Answer ^=  n;
+		 }
+		/////////////////////////////////////////////////////////////////////////////////////////////
+	
+        // Print the answer to standard output(screen).
+        
+		printf("Case #%d\n", test_case+1);
+        printf("%d\n", Answer);
+        
+	}
+
+	return 0;//Your program should return 0 on normal termination.
+}
