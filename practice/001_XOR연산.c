2aa0305545ed9b0c068ec8e10f4791b01871659e
minetoiles <163957799+minetoiles@users.noreply.github.com>
2025-07-02 00:12:11 +0900

    Update 001_XOR연산.c

--- practice/001_XOR연산.c
+++ practice/001_XOR연산.c
@@ -21,33 +21,9 @@ int Answer;
 int main(void)
 {
 	int T, test_case;
-	/*
-	   The freopen function below opens input.txt file in read only mode, and afterward,
-	   the program will read from input.txt file instead of standard(keyboard) input.
-	   To test your program, you may save input data in input.txt file,
-	   and use freopen function to read from the file when using scanf function.
-	   You may remove the comment symbols(//) in the below statement and use it.
-	   But before submission, you must remove the freopen function or rewrite comment symbols(//).
-	 */
-	// freopen("input.txt", "r", stdin);
-
-	/*
-	   If you remove the statement below, your program's output may not be rocorded
-	   when your program is terminated after the time limit.
-	   For safety, please use setbuf(stdout, NULL); statement.
-	 */
-	setbuf(stdout, NULL);
-
 	scanf("%d", &T);
 	for(test_case = 0; test_case < T; test_case++)
-	{
-		
-		/////////////////////////////////////////////////////////////////////////////////////////////
-		/*
-		   Implement your algorithm here.
-		   The answer to the case will be stored in variable Answer.
-		 */
-		 
+	{	 
 		 int num;
 		 scanf("%d", &num);
 		 Answer = 0;
@@ -57,14 +33,11 @@ int main(void)
 		     scanf("%d", &n);
 		     Answer ^=  n;
 		 }
-		/////////////////////////////////////////////////////////////////////////////////////////////
-	
-        // Print the answer to standard output(screen).
         
 		printf("Case #%d\n", test_case+1);
-        printf("%d\n", Answer);
+        	printf("%d\n", Answer);
         
 	}
 
-	return 0;//Your program should return 0 on normal termination.
+	return 0;
 }
